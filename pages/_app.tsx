5e5ec98f63cb557cbb1b0df59d903ba9fad590a5
Андрюха Едунов <opty@opty.ru>
2021-05-01 13:21:48 +0300

    feat: added page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
 import React from 'react';
 import { Layout } from '../src/components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Consents</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
